59b8c6138c9fe06785ca63d54b59307ad1c66b0b
SerhiiRud <carmagsales@gmail.com>
2023-12-20 12:06:31 +0200

    modal window

---
+++ js/modal.js
@@ -0,0 +1,17 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelectorAll("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  [...refs.openModalBtn].map((item) =>
+    item.addEventListener("click", toggleModal)
+  );
+
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
